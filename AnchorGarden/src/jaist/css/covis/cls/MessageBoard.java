a1ed05de82859a17291dbe5106b2130721aeda74
Asai <asai@sakailab.info>
2014-10-20 13:41:03 +0900

    共有のためのコミット

---
+++ AnchorGarden/src/jaist/css/covis/cls/MessageBoard.java
@@ -0,0 +1,62 @@
+package jaist.css.covis.cls;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Polygon;
+import java.awt.geom.RoundRectangle2D;
+
+import edu.umd.cs.piccolo.nodes.PPath;
+import edu.umd.cs.piccolo.nodes.PText;
+
+public class MessageBoard extends PPath {
+
+	private static final long serialVersionUID = 8131819427660903628L;
+	public static Color defaultColor = new Color(135,206,250);
+	
+	public MessageBoard(String labeltxet){
+		init(0,0,labeltxet);
+	}
+	
+	public MessageBoard(double x, double y, String labeltxet){
+		init(x,y,labeltxet);
+	}
+	
+	PPath triangle;
+	PText label;
+	
+	public void init(double x, double y, String labeltxet){
+		//表示するテキスト作成
+		String labeltxet2[];
+		//labeltxetから無駄な部分を切り取る
+		labeltxet2 = labeltxet.split(".* ");
+		//必要な部分を先頭に
+		if(labeltxet2.length >= 2) {
+			labeltxet2[0] = labeltxet2[1];
+		}
+		label = new PText(labeltxet2[0]);
+		label.scale(2.8f);
+		label.offset(20, 20);
+		addChild(label);
+		
+		//吹き出しの四角い部分作成
+		setOffset(x, y);
+		setPathTo(new RoundRectangle2D.Double(0, 0, 30+labeltxet2[0].length()*18, 100, 30, 30));
+		setPaint(defaultColor);
+		setStrokePaint(Color.lightGray);
+		setStroke(new BasicStroke(1));
+
+		//吹き出しの三角形を作成
+		int x2 = (int)x;
+		int y2 = (int)y;
+		
+		triangle = new PPath();
+		int[] pX = { x2 + 40, x2 + 40, x2 + 40 + 40 };
+		int[] pY = { y2 + 100 - 2, y2 + 100 + 40 - 2, y2 + 100 - 2};
+		triangle.setPathTo(new Polygon(pX, pY, 3));
+		triangle.setPaint(defaultColor);
+		triangle.setStrokePaint(Color.lightGray);
+		triangle.setStroke(new BasicStroke(1));
+		addChild(triangle);
+	}
+
+}
---
+++ AnchorGarden/src/jaist/css/covis/cls/MessageManager.java
@@ -0,0 +1,70 @@
+package jaist.css.covis.cls;
+
+
+import jaist.css.covis.CoVisBuffer;
+
+import java.lang.reflect.Method;
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+import edu.umd.cs.piccolo.PLayer;
+import edu.umd.cs.piccolo.PNode;
+
+/**
+ * @author macchan EPMessageManager
+ */
+public class MessageManager {
+
+	static CoVisBuffer buffer;
+	
+	public static Object sendMessage(CoVisBuffer buf,String method, PNode sender, PNode receiver, boolean isReturn) {
+
+		buffer = buf;
+		
+		//アニメーション再生
+		animation(method, sender, receiver, isReturn);
+
+		return null;
+	}
+
+	private static void animation(String message, PNode sender,
+			PNode receiver, boolean isReturn) {
+		
+		try {
+			//動かす吹き出しを作成
+			MessageBoard board = new MessageBoard(message);
+
+			// 返り値だったらメッセージボードの色を変える
+			if(isReturn){
+				board.setPaint(new Color(255,204,204));
+				board.triangle.setPaint(new Color(255,204,204));
+			}
+			
+			//吹き出しをレイヤーに追加
+			buffer.layer.addChild(board);
+			
+			//送信側と受信側の位置を取得
+			Point2D src = sender.getGlobalBounds().getCenter2D();
+			Point2D dest = receiver.getGlobalBounds().getCenter2D();
+
+			double frameNumber = 20;
+			double dx = (dest.getX() - src.getX()) / frameNumber;
+			double dy = (dest.getY() - src.getY()) / frameNumber;
+
+			//吹き出しの初期位置設定
+			board.setOffset(src.getX(), src.getY() - 150);
+			
+			//アニメーション部分
+			for (int i = 0; i < frameNumber; i++) {
+				board.translate(dx, dy);
+				board.repaint();
+				Thread.sleep(100);
+			}
+
+			//追加した吹き出しを削除
+			buffer.layer.removeChild(board);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
